37da34e2ca93b4159d32b7a61f85654792e97a12
CarlyCoding <carly_off@outlook.com>
2022-08-05 18:59:48 +0100

    folder structure created

---
+++ src/containers/FilmsContainer.js
@@ -0,0 +1,12 @@
+import {useState, useEffect} from 'react'
+// Future imports here
+
+const FilmsContainer = () => {
+
+    const [films, setFilms] = useState([])
+    // Set selector here 
+    
+
+
+
+}
