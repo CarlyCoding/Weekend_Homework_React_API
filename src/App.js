35b371e50e3d917612856e11c57f75209aa746b2
CarlyCoding <carly_off@outlook.com>
2022-08-05 19:34:54 +0100

    fixed version issues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import FilmsContainer from './containers/FilmsContainer'
 
 function App() {
-// Return function with container here. 
+return (
+  <FilmsContainer/>
+  );
 }
 
 export default App;
+
--- src/containers/FilmsContainer.js
+++ src/containers/FilmsContainer.js
@@ -5,8 +5,21 @@ const FilmsContainer = () => {
 
     const [films, setFilms] = useState([])
     // Set selector here 
-    
 
 
+    useEffect(() => {
+      getFilms()
+        
+    }, [])
 
+    const getFilms = () => {
+        fetch("https://ghibliapi.herokuapp.com/films")
+        .then(res => res.json())
+        .then(res => console.log(res))
+
+    }
+
+    return (null)
 }
+
+export default FilmsContainer;
